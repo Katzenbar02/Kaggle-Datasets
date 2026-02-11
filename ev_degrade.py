92a0f40a884582352688d03f658151243dd18e63
Joshua Ludwig <joshualudwig02@gmail.com>
2026-02-11 12:14:54 -0700

    degrade

--- ev_degrade.py
+++ ev_degrade.py
@@ -2,6 +2,8 @@
 import pandas as pd
 import sqlite3
 import altair as alt
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 data = pd.read_csv('kaggle/ev_battery_degradation_v1.csv')
 conn = sqlite3.connect(':memory:')
@@ -37,4 +39,35 @@ chart = alt.Chart(degrade_data).mark_line(point=True).encode(
     height=400
 )
 chart.show()
+
+#%% what is the average decrease in SoH_Percent for each Battery_Type every month
+sql_query2 = """
+SELECT Battery_Type, AVG(SoH_Percent) AS Avg_SoH, AVG(Vehicle_age_Months) AS Avg_Age_Months
+FROM ev_degrade
+GROUP BY Battery_Type
+"""
+degrade_summary = pd.read_sql(sql_query2, conn)
+degrade_summary['Avg_Decrease_Per_Month'] = degrade_summary['Avg_SoH'] / degrade_summary['Avg_Age_Months']
+print(degrade_summary[['Battery_Type', 'Avg_Decrease_Per_Month']])
+
+
+
+
+
+
+
+
+
+
+# %%
+#  Heatmap of the ev_degradation data to see the correlation between the different features remove the non numeric features
+numeric_data = data.select_dtypes(include=['float64', 'int64'])
+correlation_matrix = numeric_data.corr()
+plt.figure(figsize=(10, 8))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt='.2f', linewidths=0.5)
+plt.title('Correlation Heatmap of EV Degradation Data')
+plt.show()
+
+
+
 # %%
